fb937382476d63eb03edbc534c71191a204c758c
Wave <torrent_den2018@mail.ru>
2025-05-19 13:13:56 +0000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useState } from 'react';
 import './App.css';
+import getWaveBlob from './wavBlobUtil';
+var microOff = require('./resources/micro_off.png');
+var microOn = require('./resources/micro_on.png');
 
-function App() {
+var mediaRecorder : MediaRecorder | null;
+var chunks: BlobPart[] | undefined = [];
+
+
+const App : FC = () => {
+  const [microImg, setMicroImg] = useState(microOff)
+
+  var stream : MediaStream;
+  let audioBlob : Blob;
+
+  const onClickMicro = async () =>
+  {
+    if (!navigator.mediaDevices?.getUserMedia({audio: true}) && !navigator.mediaDevices) {
+      return console.warn('Not supported')
+    }
+    if (microImg === microOff)
+    {
+      setMicroImg(microOn);
+      try
+      {
+        stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = async (e) =>
+        {
+          chunks?.push(e.data);
+          audioBlob = new Blob(chunks, { type: "audio/webm" });
+          await saveRecord(audioBlob);//await getWaveBlob(audioBlob, false, { sampleRate: 4000})
+        }
+        mediaRecorder.start();
+      }
+      catch(error)
+      {
+        console.error("error", error);
+      }
+    }
+    else
+    {
+      mediaRecorder?.stop();
+      
+      setMicroImg(microOff);
+      chunks = [];
+      mediaRecorder = null;
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>OK</h1>
+      <img src={ microImg } alt="micro" onClick={onClickMicro}/>
     </div>
   );
+}
+
+const saveRecord = async (audioBlob : Blob) =>
+{
+  const formData = new FormData();
+  let audioName = Date.now();
+  formData.append("audio", audioBlob, audioName.toString());
+  try
+  {
+    await fetch("http://localhost:8000/api/audio", 
+      {
+        method: "POST",
+        body: formData
+      }
+    );
+  }
+  catch(e)
+  {
+    console.error(e);
+  }
 }
 
 export default App;
